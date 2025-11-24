7644b4fdcf70e2d939f114ca32c8e6ecb66f8552
Felixwijaya04 <felixwijaya2004@gmail.com>
2025-11-24 17:00:03 +0700

    Aura Player Controller

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -2,4 +2,28 @@
 
 
 #include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
 
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	/** check is an assertion, if the result is null the game will crash different from using if statements*/
+	check(AuraContext)
+
+	UEnhancedInputLocalPlayerSubsystem* Subsytem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsytem);
+	Subsytem->AddMappingContext(AuraContext,0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Type::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
