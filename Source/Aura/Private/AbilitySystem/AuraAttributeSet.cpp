2dbd2dd64280d2cdbfc76e474edfc72c055686a3
Felixwijaya04 <felixwijaya2004@gmail.com>
2025-12-06 18:42:30 +0700

    Attribute Accessor

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -2,12 +2,15 @@
 
 
 #include "AbilitySystem/AuraAttributeSet.h"
-#include "AbilitySystemComponent.h"
 #include "Net/UnrealNetwork.h"
 
 UAuraAttributeSet::UAuraAttributeSet()
 {
-	
+	// Macro functions set by property name
+	InitHealth(100.0f);
+	InitMaxHealth(100.0f);
+	InitMana(75.0f);
+	InitMaxMana(75.0f);
 }
 
 void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
