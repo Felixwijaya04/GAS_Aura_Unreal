3bc8a04b7169ad1953e9ed6bc5550a58e0607e77
Felixwijaya04 <felixwijaya2004@gmail.com>
2026-02-03 19:14:25 +0700

    Listening for attribute changes

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -7,7 +7,7 @@
 UAuraAttributeSet::UAuraAttributeSet()
 {
 	// Macro functions set by property name
-	InitHealth(100.0f);
+	InitHealth(50.0f);
 	InitMaxHealth(100.0f);
 	InitMana(75.0f);
 	InitMaxMana(75.0f);
